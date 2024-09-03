3380a81170a41342d9cbfb51b36d941859da6bb6
Dannytop2001 <oketolad@gmail.com>
2024-09-03 16:59:23 +0100

    backshot

---
+++ mine.js
@@ -0,0 +1,1 @@
+You will have a local Git repository with a single file (hello.txt) committed. This repository will be pushed to GitHub, where the file will be visible in your new LocalRepoProject repository.
